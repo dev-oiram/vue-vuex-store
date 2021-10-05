20142b121fc2d0b2f183a8a5e933d71522c75c8e
Mario <oiram.251170@gmail.com>
2021-10-05 17:10:05 -0500

    Agregar provider
    Se agrega globalstore en main.js como provider

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
+import globalstore from '@/globalstore'
 
-createApp(App).use(store).mount('#app')
+createApp(App).provide('store',globalstore).use(store).mount('#app')

3cd9512b50aa3cb1d914e325db14ed16f8b5017b
Mario <oiram.251170@gmail.com>
2021-10-05 18:14:17 -0500

    Agregan metodos
    Se agregan metodos: incrementar, decrementar y reiniciar

--- src/components/componentone.vue
+++ src/components/componentone.vue
@@ -2,6 +2,9 @@
     <div>
         <h3>Componente - UNO</h3>
         <p>Resultado: {{ store.state.contador }}</p>
+        <button @click="store.incrementar">Incrementar</button>
+        <button @click="store.decrementar">Decrementar</button>
+        <button @click="store.reiniciar">Reiniciar</button>
     </div>
 </template>
 
